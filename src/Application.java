a6b625800739143593e9b18e4b62a33e893805e3
CHAITU SAINI <CHAITU SAINI@CHAITU>
2017-07-27 11:25:42 -0700

    Java Variables

---
+++ src/Application.java
@@ -0,0 +1,27 @@
+
+public class Application {
+	public static void main(String[] args) {
+		int myNumber = 45;
+		short myShort = 487;
+		long myLong = 454545;
+
+		double myDouble = 7.323;
+		float myFloat = 324.3f;
+
+		char myChar = 'D';
+		boolean myBoolean = false;
+
+		byte myByte = 119;
+
+		System.out.println(myNumber);
+		System.out.println("Hello World");
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+	}
+
+}
